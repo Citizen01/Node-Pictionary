f0f430d93deb56526e1b67cb997dd65b9531bf34
Citizen01 <jeanbaptiste.e@hotmail.fr>
2014-01-18 15:26:17 +0100

    Comments into the start script + check db folder.
    - Added comments into the start script file.
    - The script will now check if the db folder exists, and will create it
    if it doesn't.

--- start.bat
+++ start.bat
@@ -1,2 +1,12 @@
+REM This script will create the db folder for mongodb if it doesn't exist,
+REM It will then start the mongodb server and then the node server in 2 different cmd window.
+
+IF EXIST ./db GOTO START_SERVERS
+	MKDIR db
+
+:START_SERVERS
+REM Start the mongodb server
 START cmd /C mongod --dbpath ./db
+
+REM Start the node server
 START cmd /K nodemon -e js,html,css app.js
